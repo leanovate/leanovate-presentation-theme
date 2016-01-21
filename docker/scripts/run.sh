94f2d3a87f35b512e92124c90035407466bab85c
Untoldwind <landru@untoldwind.net>
2016-01-21 19:59:52 +0100

    Add server mode

--- docker/scripts/run.sh
+++ docker/scripts/run.sh
@@ -4,6 +4,13 @@ case $1 in
 sh)
     /bin/sh
     ;;
+server)
+    rsync --daemon --config=/etc/rsyncd.conf
+    while true ; do
+      hugo server --bind "0.0.0.0"
+      sleep 2
+    done
+    ;;
 *)
     hugo $@
     ;;
